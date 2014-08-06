bee7464f93484d20783a73a1b5906a8aaedec15f
Barbara Wahl <wahl@hanover.edu>
2014-08-06 12:21:48 -0400

    Implementation of dot product for vectors

--- linAlg/vector.js
+++ linAlg/vector.js
@@ -1,6 +1,10 @@
 (function(define) {'use strict';
 define(function(require) {
 
+	// TODO: decide if Vectors are editable
+	// 
+	// TODO:  Testing
+
 	var Vector, DenseV, SparseV;
 
 	/**
@@ -29,12 +33,31 @@ define(function(require) {
 	}
 
 	/**
+	 * Dot product
+	 * @param  {[type]} other [description]
+	 * @return {[type]}       [description]
+	 */
+	Vector.prototype.dot = function dot(other) {
+		var res, i, l;
+
+		res = 0;
+		l = this.length;
+
+		for(i = 0; i < l; i += 1) {
+			res += this.get(i) * other.get(i);
+		}
+
+		return res;
+	};
+
+	/**
 	 * Constructs a DenseV object
 	 * @param {array} arr - Values
 	 */
 	function DenseV(arr) {
 		this.values = arr;
 		this.length = arr.length;
+		this.nnz = this.length;
 	}
 
 	DenseV.prototype = Object.create(Vector.prototype);
@@ -42,6 +65,14 @@ define(function(require) {
 	// DenseV.prototype methods
 	DenseV.prototype.get = function get(i) {
 		return this.values[i] || 0;
+	}
+
+	DenseV.prototype.dot = function dot(other) {
+		if (isSparse(other)) {
+			return other.dot(this);
+		}
+		// both are dense
+		return Vector.prototype.dot.call(this, other);
 	}
 
 	/**
@@ -54,6 +85,8 @@ define(function(require) {
 	function SparseV(arr, len) {
 		this.values = arr;
 		this.length = len;
+		this.keys = arr.keys(); // array of nonzero entries
+		this.nnz = this.keys.length; // number of nonzero entries
 	}
 
 	SparseV.prototype = Object.create(Vector.prototype);
@@ -62,6 +95,41 @@ define(function(require) {
 
 	SparseV.prototype.get = function get(i) {
 		return this.values[i] || 0;
+	}
+
+	SparseV.prototype.dot = function dot(other) {
+		// "this" is sparse
+		// "other" is sparse or dense
+		var res, i, l, that;
+
+		res = 0;
+		that = this;
+
+		if (that.nnz > other.nnz) { 
+			// swap roles
+			that = other;
+			other = this;
+		}
+
+		l = that.nnz;
+
+		for(i = 0; i < l; i += 1) {
+			res += that.get(that.keys[i]) * other.get(that.keys[i]);
+		}
+
+		return res;
+	};
+
+	// Helper functions
+	
+	/** @private */
+	function sameLength(a, b) {
+		return a.length === b.length;
+	}
+
+	/** @private */
+	function isSparse(a) {
+		return a instanceof SparseV;
 	}
 
 });
