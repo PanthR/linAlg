638d64a5c4ef65c868e429ccc897b59bbe05c189
Haris Skiadas <cskiadas@gmail.com>
2014-08-12 20:16:38 -0400

    Simplify mocha test stack traces
    Relative paths from project top, and only last function call.

---
+++ test/stackFixer.js
@@ -0,0 +1,21 @@
+var oldconsoleError = console.error, errorStackDepth = 1;
+var path = require('path');
+var appRoot = path.resolve(__dirname, '..')+'/';
+var regexp = /\.spec\.js\:\d+\:\d+/;
+
+console.error = function () {//fmt, i, title, msg, stack
+   var args = Array.prototype.slice.call( arguments, 0 ), stack = args[ 4 ], match;
+
+   if ( stack ) {
+      while (stack.indexOf(appRoot) > -1) {
+         stack = stack.replace(appRoot, '', 'mg');
+      }
+      match = stack.match(regexp);
+      if (match) {
+         stack = stack.split('\n')[0] + '\n';
+      }
+      args[ 4 ] = stack;
+   }
+
+   oldconsoleError.apply( console, args );
+};
