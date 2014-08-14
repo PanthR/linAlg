139997545ecd632d264603b5720bc501e6e49dc8
Haris Skiadas <cskiadas@gmail.com>
2014-08-14 01:22:26 -0400

    Add basic tests for Vector.map

--- test/vector/iterators.spec.js
+++ test/vector/iterators.spec.js
@@ -87,4 +87,43 @@ describe('Iterations over vectors via', function() {
       });
       
    });
+   describe('map', function() {
+      it('provides the correct values', function() {
+         var a;
+         var f = function(v, i) { a.push(v); };
+         a = []; v1.map(f);        expect(a).to.deep.equal([4, 2, 1]);
+         a = []; v2.map(f, false); expect(a).to.deep.equal([0, 4, 0, 0, 1, 2, 0, 0, 0, 0]);
+         a = []; v2.map(f, true);  expect(a).to.deep.equal([4, 1, 2]);
+         a = []; v3.map(f);        expect(a).to.deep.equal([1, 4, 9, 16]);
+      });
+      it('provides the correct indices', function() {
+         var a;
+         var f = function(v, i) { a.push(i); }
+         a = []; v1.map(f);       expect(a).to.deep.equal([1, 2, 3]);
+         a = []; v2.map(f);       expect(a).to.deep.equal([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+         a = []; v2.map(f, true); expect(a).to.deep.equal([2, 5, 6]);
+         a = []; v3.map(f);       expect(a).to.deep.equal([1, 2, 3, 4]);
+      });
+      it('returns a vector of the same length', function() {
+         var f = function(v, i) { return v + i; }
+         expect(v1.map(f)).to.be.instanceof(Vector);
+         expect(v1.map(f).length).to.equal(v1.length);
+         expect(v2.map(f)).to.be.instanceof(Vector);
+         expect(v2.map(f).length).to.equal(v2.length);
+         expect(v2.map(f, true).length).to.equal(v2.length);
+         expect(v3.map(f)).to.be.instanceof(Vector);
+         expect(v3.map(f).length).to.equal(v3.length);
+      });
+      it('the resulting vector has the correct values', function() {
+         var f = function(v, i) { return v + i; }
+         expect(v1.map(f).get(1)).to.equal(5);
+         expect(v1.map(f).get(2)).to.equal(4);
+         expect(v1.map(f).get(3)).to.equal(4);
+         expect(v2.map(f).get(2)).to.equal(6);
+         expect(v2.map(f, true).get(2)).to.equal(6);
+         expect(v2.map(f).get(1)).to.equal(1);
+         expect(v2.map(f, true).get(1)).to.equal(0);
+         expect(v3.map(f).get(2)).to.equal(6);
+      });
+   });
 });
