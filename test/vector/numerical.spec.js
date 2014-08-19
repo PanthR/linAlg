cfa44b80649cd329c426034fe601f2738f91dee4
Haris Skiadas <cskiadas@gmail.com>
2014-08-19 11:32:03 -0400

    Add Vector.dot tests

--- test/vector/numerical.spec.js
+++ test/vector/numerical.spec.js
@@ -6,6 +6,9 @@ describe('Numerical computations', function() {
    var v1 = new Vector([4, -2, 1]);
    var v2 = new Vector({ 6: -2, 2: 4, 5: 1 }, 10);
    var v3 = new Vector(function(x) { return -x*x; }, 4);
+   var v4 = new Vector([1, 2, 3]);
+   var v5 = new Vector({ 2: 1 }, 3);
+   var v6 = new Vector(function(x) { return x; }, 3);
 
    it('norm', function() {
       expect(v1.norm(Infinity)).to.equal(4);
@@ -19,5 +22,15 @@ describe('Numerical computations', function() {
       expect(v3.norm()).to.be.closeTo(Math.sqrt(1+4*4+9*9+16*16), delta);
       expect(v1.norm(3)).to.be.closeTo(4.17933919, delta);
       expect(v1.norm(1/3)).to.be.closeTo(56.947628, delta);
+   });
+   it('dot', function() {
+      expect(v1.dot(v1)).to.equal(21);
+      expect(v2.dot(v2)).to.equal(21);
+      expect(v3.dot(v3)).to.equal(1+4*4+9*9+16*16);
+      expect(v1.dot(v4)).to.equal(3);
+      expect(v1.dot(v5)).to.equal(-2);
+      expect(v1.dot(v6)).to.equal(3);
+      expect(v5.dot(v1)).to.equal(-2);
+      expect(v6.dot(v1)).to.equal(3);
    });
 });
