075e307482bf9ed54459bb8142156f54367883f6
Barbara Wahl <wahl@hanover.edu>
2014-08-21 11:26:21 -0400

    Tests for constant vectors.

---
+++ test/vector/const.spec.js
@@ -0,0 +1,22 @@
+var Vector = require('../../linAlg/vector');
+var ConstV = Vector.ConstV;
+var expect = require('chai').expect;
+
+describe('Constant vectors', function() {
+   it('can be constructed with Vector.const', function() {
+      var v1 = Vector.const(5,3);
+      expect(v1.length).to.equal(3);
+      expect(v1.get(2)).to.equal(5);
+      expect(v1.get(-2)).to.equal(0);
+      expect(v1.get(4)).to.equal(0);
+      expect(v1).to.be.instanceof(ConstV);
+   });
+      it('can be constructed with Vector.ones', function() {
+      var v1 = Vector.ones(3);
+      expect(v1.length).to.equal(3);
+      expect(v1.get(2)).to.equal(1);
+      expect(v1.get(-2)).to.equal(0);
+      expect(v1.get(4)).to.equal(0);
+   });
+   
+});
