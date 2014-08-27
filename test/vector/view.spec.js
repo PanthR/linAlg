9520af3817f039ac0a494964aea232e8d9f4fe8f
Haris Skiadas <cskiadas@gmail.com>
2014-08-27 18:17:38 -0400

    Add tests for Vector#view

--- test/vector/view.spec.js
+++ test/vector/view.spec.js
@@ -4,7 +4,7 @@ var expect = require('chai').expect;
 
 describe('View vectors', function() {
    var target = new Vector([5, 4, 3, 2, 1]);
-   var v1 = new ViewV(target, [1, 4]); // [5, 2, 2]
+   var v1 = new ViewV(target, [1, 4, 4]); // [5, 2, 2]
    var v2 = new ViewV(target, function(i) { return 6-i; }, 5); // reverse
 
    it('have a constructor Vector.ViewV and are Vector\'s', function() {
@@ -15,8 +15,18 @@ describe('View vectors', function() {
       expect(v2).to.be.instanceof(Vector);
    });
    it('have the correct values', function() {
-      expect(v1.toArray()).to.deep.equal([5, 2]);
+      expect(v1.toArray()).to.deep.equal([5, 2, 2]);
       expect(v2.toArray()).to.deep.equal([1, 2, 3, 4, 5]);
    });
-
+   it('iterate based on their indices, not the target\'s', function() {
+      var a = [];
+      function f(val, i) { a.push([val, i]); }
+      v1.each(f);
+      expect(a).to.deep.equal([[5, 1], [2, 2], [2, 3]]);
+   });
+   it('can be created via Vector#view', function() {
+      expect(target).to.respondTo('view');
+      var v3 = target.view([1, 4, 4]);
+      expect(v3.toArray()).to.deep.equal([5, 2, 2]);
+   });
 });
