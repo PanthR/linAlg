ad2b6a3469532c26ace4f821cde2fb50e8a3fb7e
Haris Skiadas <cskiadas@gmail.com>
2014-08-30 21:37:53 -0400

    Try a simplified version of DenseM construction

--- linAlg/matrix/dense.js
+++ linAlg/matrix/dense.js
@@ -11,35 +11,21 @@ return function(Matrix) {
       if (arr.length === 0) { throw new Error('Cannot create empty matrix yet.'); }
       if (typeof options === 'boolean') { options = { byRow: options }; }
       if (options == null) { options = {}; }
-      this.byRow = options.byRow === true;
+      this.byRow = options.byRow === true;  // byRow defaults to false
       if (Array.isArray(arr[0])) {
-         setFromDoubleArray(this, arr, options.byRow);
-      } else {
-         setFromSingleArray(this, arr, options);
+         // If array of arrays, set an options parameter and flatten the array
+         // The other dimension can be deduced from overall length
+         // Assumes arrays are equal length.
+         options[this.byRow ? 'nrow' : 'ncol'] = arr.length;
+         arr = [].concat.apply([], arr);
       }
+      this.values = new Matrix.Vector(arr);
+      this.nrow = options.nrow || Math.floor(arr.length / options.ncol);
+      this.ncol = options.ncol || Math.floor(arr.length / this.nrow);
       if (this.ncol * this.nrow !== this.values.length) {
          throw new Error('Declared matrix dimensions invalid');
       }
       return this;
-
-      function setFromDoubleArray(obj, arr, byRow) {
-         obj.values = new Matrix.Vector([].concat.apply([], arr));
-         if (byRow) {
-            obj.nrow = arr.length; obj.ncol = arr[0].length;
-         } else {
-            obj.nrow = arr[0].length; obj.ncol = arr.length;
-         }
-      }
-      function setFromSingleArray(obj, arr, options) {
-         obj.values = new Matrix.Vector(arr);
-         if (options.nrow == null) {
-            obj.ncol = options.ncol;
-            obj.nrow = Math.floor(arr.length / options.ncol);
-         } else if (options.ncol == null) {
-            obj.nrow = options.nrow;
-            obj.ncol = Math.floor(arr.length / options.nrow);
-         }
-      }
    }
 
    DenseM.prototype = Object.create(Matrix.prototype);
