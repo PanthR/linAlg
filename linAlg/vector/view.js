47b644a13371ba24fad0c918083a290c989d5e5b
Haris Skiadas <cskiadas@gmail.com>
2014-09-05 09:21:25 -0400

    Remove the superfluous ViewV subclasses. Close #66

--- linAlg/vector/view.js
+++ linAlg/vector/view.js
@@ -9,23 +9,16 @@ return function(Vector) {
    // Subclass of `Vector` representing vectors that provide a "view" into
    // another object, e.g. the row or column of a `Matrix`. Changes to a view
    // vector cause changes to the corresponding "viewed" object and vice versa.
-
-   // `target` can be either a Vector or a Matrix.
-   function ViewV(target, indices, len) {
-      if (target instanceof Vector) {
-         return new VectorView(target, indices, len);
-      }
-      return new MatrixView(target, indices, len, arguments[3]);
-   }
-
+   //
    // Constructs a view into a `Vector`. `target` must be a `Vector`.
    // `indices` can be:
    // 1. an array of indices, or
-   // 2. a function which computes the translation from VectorView-index
+   // 2. a function which computes the translation from ViewV-index
    // to target-index
    //
    // `len` is the length of the resulting vector. Needed only in case 2.
-   function VectorView(target, indices, len) {
+   function ViewV(target, indices, len) {
+      if (!this instanceof ViewV) { return new ViewV(target, indices, len); }
       this.target = target;
       if (typeof indices === 'function') {
          this.i = indices;
@@ -41,36 +34,20 @@ return function(Vector) {
       return this;
    }
 
-   // Constructs a view into a `Matrix`. `target` must be a `Matrix`.
-   // `rowIndex` and `colIndex` can be:
-   // 1. A positive integer. Then the view is into elements in that row/column
-   // 2. A array of indices, or
-   // 3. A function for calculating indices.
-   //
-   // len is optional if at least one of `rowIndex` and `colIndex` is an array.
-   function MatrixView(target, rowIndex, colIndex, len) {
-      this.target = target;
-      function lookup(indices) {
-         if (typeof indices === 'function') { return indices; }
-         if (Array.isArray(indices)) {
-            return function(i) { return indices[i - 1]; };
-         }
-         return function(i) { return indices; }; // constant
-      }
-      this.i = lookup(rowIndex);
-      this.j = lookup(colIndex);
-      this.length = rowIndex.length || colIndex.length || len || 0;
-      this.cached = false;
-      return this;
-   }
-
    ViewV.prototype = Object.create(Vector.prototype);
-   VectorView.prototype = Object.create(ViewV.prototype);
-   MatrixView.prototype = Object.create(ViewV.prototype);
 
    ViewV.prototype._get = function _get(i) {
       if ( i < 1 || i > this.length) { return 0; }
       return this.compute(i);
+   };
+
+   ViewV.prototype.compute = function compute(i) {
+      return this.target._get(this.i(i));
+   };
+
+   ViewV.prototype.change = function change(i, val) {
+      this.target._set(this.i(i), val);
+      return this;
    };
 
    // A ViewV's mutability is directly tied to its target's mutability.
@@ -88,24 +65,6 @@ return function(Vector) {
          f(this._get(i), i);
       }
       return Vector;
-   };
-
-   VectorView.prototype.compute = function compute(i) {
-      return this.target._get(this.i(i));
-   };
-
-   VectorView.prototype.change = function change(i, val) {
-      this.target._set(this.i(i), val);
-      return this;
-   };
-
-   MatrixView.prototype.compute = function compute(i) {
-      return this.target._get(this.i(i), this.j(i));
-   };
-
-   MatrixView.prototype.change = function change(i, val) {
-      this.target._set(this.i(i), this.j(i), val);
-      return this;
    };
 
    return ViewV;
