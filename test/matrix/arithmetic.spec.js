4e86ed2de628dde6c258f78bac6791884b4532fd
Haris Skiadas <cskiadas@gmail.com>
2014-09-16 13:23:12 -0400

    Add tests for Matrix#sMult. Close #104

--- test/matrix/arithmetic.spec.js
+++ test/matrix/arithmetic.spec.js
@@ -3,17 +3,17 @@ var expect = require('chai').expect;
 
 function random(i) { return Math.floor(Math.random() * i + 1); }
 
-describe('Matrix addition', function() {
-   var A = [];
-   A[1] = new Matrix([4, 6, 7, 2, 1, 3, 5, 7.53, 9.95], { nrow : 3 });
-   A[2] = new Matrix({ 2: { 3: 8, 1: 2}, 1: { 1: 5 }}, { nrow : 3, ncol: 3 });
-   A[3] = new Matrix(function(i, j) { return i * j; }, { nrow: 3, ncol: 3 });
-   A[4] = Matrix.diag([4, 3.3, 5.1]);
-   A[5] = Matrix.const(5.8, 3);
-   A[6] = new Matrix.StructuredM.LowerTriM(A[1]);
-   A[7] = new Matrix.StructuredM.UpperTriM(A[1]);
-   A[8] = new Matrix.StructuredM.SymmetricM(A[1]);
+var A = [];
+A[1] = new Matrix([4, 6, 7, 2, 1, 3, 5, 7.53, 9.95], { nrow : 3 });
+A[2] = new Matrix({ 2: { 3: 8, 1: 2}, 1: { 1: 5 }}, { nrow : 3, ncol: 3 });
+A[3] = new Matrix(function(i, j) { return i * j; }, { nrow: 3, ncol: 3 });
+A[4] = Matrix.diag([4, 3.3, 5.1]);
+A[5] = Matrix.const(5.8, 3);
+A[6] = new Matrix.StructuredM.LowerTriM(A[1]);
+A[7] = new Matrix.StructuredM.UpperTriM(A[1]);
+A[8] = new Matrix.StructuredM.SymmetricM(A[1]);
 
+describe('Matrix addition', function() {
    function execute(f) { // do f for each pair in A
       for (var i = 1; i < A.length; i += 1) {
          for (var j = 1; j < A.length; j += 1) {
@@ -65,6 +65,36 @@ describe('Matrix addition', function() {
          expect(function() {
             m.view([1, 2],[1, 2, 3]).pAdd(n, 2);
          }).to.throw(Error);
+      });
+   });
+});
+describe('Scalar multiplication', function() {
+   function execute(f) {
+      for (var i = 1; i < A.length; i += 1) {
+            f(A[i]);
+      }
+   }
+   it('exists', function() {
+      execute(function(m) {
+         var m2;
+         expect(m).to.respondTo('sMult');
+         expect(function() { m2 = m.sMult(Math.random()); }).to.not.throw(Error);
+         expect(Matrix.sameDims(m, m2)).to.be.true;
+      });
+   });
+   it('has the correct values', function() {
+      execute(function(m) {
+         var k = Math.random(), m2 = m.sMult(k);
+         for (var i = 1; i <= m.nrow; i += 1) {
+            for (var j = 1; j <= m.ncol; j += 1) {
+               expect(m2.get(i, j)).to.equal(k * m.get(i, j));
+            }
+         }
+      });
+   });
+   it('preserves structure', function() {
+      execute(function(m) {
+         expect(m.sMult(Math.random()).classes[0]).to.equal(m.classes[0]);
       });
    });
 });
